c254c339e42d3c82e82dd79f628add18bbe44fc2
捷成吴 <jasonwood2031@gmail.com>
2017-12-12 20:57:58 +0800

    Some new files...

--- src/data/easy_integer_range2.c
+++ src/data/easy_integer_range2.c
@@ -1,14 +1,7 @@
-#include<stdio.h>
-
 int main(){
 	int localVar = 8;
-
-	localVar=localVar<<5;
-	printf("%d\n", localVar);
-	localVar=localVar>>2;
-	printf("%d\n", localVar);
-	localVar=localVar<<30;
-	printf("%d\n", localVar);
-
+	localVar = localVar << 5;
+	localVar = localVar >> 2;
+	localVar = localVar << 30;
 	return 0;
 }
